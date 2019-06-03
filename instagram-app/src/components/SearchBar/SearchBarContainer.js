df736e74d985dc1c5d67ec15531c1635ec917d0c
BenR <thisbenrogers@gmail.com>
2019-06-03 17:30:36 -0500

    removed svg, refactored searchbar to use styled components

--- instagram-app/src/components/SearchBar/SearchBarContainer.js
+++ instagram-app/src/components/SearchBar/SearchBarContainer.js
@@ -1,19 +1,90 @@
 import React from "react";
+import styled from "styled-components";
 
 import "./SearchBar.css";
 
-class SearchBarContainer extends React.Component {
-  render() {
-    return (
-      <div className="search-container">
-        <p>logo | Instagram</p>
-        <form>
-          <input type="text" />
-        </form>
-        <p>Icons</p>
-      </div>
-    );
+const Nav = styled.div`
+  background-color: #fff;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.0975);
+`;
+
+const NavHeader = styled.div`
+  max-width: 1010px;
+  padding: 26px 20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const NavLeft = styled.div`
+  width: 33.333%;
+  text-align: left;
+`;
+
+const NavCenter = styled.div`
+  width: 33.333%;
+  text-align: center;
+`;
+
+const Input = styled.input`
+  font-size: 16px;
+  border: solid 1px #dbdbdb;
+  border-radius: 3px;
+  color: #262626;
+  padding: 7px 33px;
+  border-radius: 3px;
+  color: #999;
+  cursor: text;
+  font-size: 14px;
+  font-weight: 300;
+  text-align: center;
+  background: #fafafa;
+
+  &:active,
+  &:focus {
+    text-align: left;
   }
+`;
+
+const NavRight = styled.div`
+  display: flex;
+  width: 33.333%;
+  text-align: right;
+
+  p {
+    margin-right: 20px;
+  }
+`;
+
+const MenuLink = styled.a``;
+
+function SearchBarContainer() {
+  return (
+    <Nav>
+      <NavHeader>
+        <NavLeft>Instagram</NavLeft>
+
+        <NavCenter>
+          <Input type="text" placeholder="Search" />
+        </NavCenter>
+
+        <NavRight>
+          <MenuLink href="#">
+            <p>compass</p>
+          </MenuLink>
+
+          <MenuLink href="#">
+            <p>heart</p>
+          </MenuLink>
+
+          <MenuLink href="#">
+            <p>user</p>
+          </MenuLink>
+        </NavRight>
+      </NavHeader>
+    </Nav>
+  );
 }
 
 export default SearchBarContainer;
